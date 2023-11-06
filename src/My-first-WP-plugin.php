72941a29adde2c2e820eba767634b38c9b8e2521
Florian Graff <florian.graff@klosebrothers.de>
2023-11-06 15:00:43 +0100

    added basic wp plugin

---
+++ src/My-first-WP-plugin.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * Plugin Name:       My first WP plugin v 1.0
+ * Plugin URI:        https://example.com/plugins/the-basics/
+ * Description:       Handle the basics with this plugin.
+ * Version:           0.0.1
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Florian Graff
+ * Author URI:        https://author.example.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://example.com/my-plugin/
+ * Text Domain:       my-basics-plugin
+ * Domain Path:       /languages
+ */
